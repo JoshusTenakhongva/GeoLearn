74c8b138061b404b8be7d7417dc780ac00581f03
JoshusTenakhongva <jtt75@nau.edu>
2020-01-30 12:41:12 -0700

    added csv creation and uploading to gogole drive

--- biodiversity_script_geolearn.py
+++ biodiversity_script_geolearn.py
@@ -1,15 +1,26 @@
 import mysql.connector
+import csv
 from PIL import Image
 from shapely.geometry import Polygon
 from shapely.geometry import MultiPolygon
 from shapely.geometry import Point
+from datetime import datetime
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 
 def main():
 
+	# constant for testing database executions 
 	ROWS_TO_ACCESS = 2000
-	SEARCH_RADIUS = 2
+	
+	# The size of the area that we want to search for 
+	# 1 about equals 70 miles 
+	SEARCH_RADIUS = 1
+	
+	# Our boolean that maintains the main loop 
 	getting_info = True
 
+	# connect to our database
 	mydb = mysql.connector.connect(
 	
 		host="ecolocation.c09lpapromur.us-east-2.rds.amazonaws.com",
@@ -19,6 +30,7 @@ def main():
 		database="ecolocation_data"
 		)
 
+	# Create a cursor to look through the database
 	mycursor = mydb.cursor( buffered=True )
 	
 	# Grab the descriptors of the mammals information from the database
@@ -31,6 +43,7 @@ def main():
 	animal_info = []
 	
 	#mycursor.execute( "SELECT AsText(boundaries) FROM iucn LIMIT %d" % (ROWS_TO_ACCESS) )
+	print( "starting animal boundary shape file creation" )
 	mycursor.execute( "SELECT AsText(boundaries) FROM iucn" )
 	for animal in mycursor:
 		animal_boundaries.append( create_shape( animal ) )
@@ -38,6 +51,7 @@ def main():
 	
 	#mycursor.execute( "SELECT * FROM iucn LIMIT %d" % (ROWS_TO_ACCESS) )
 	mycursor.execute( "SELECT * FROM iucn" )
+	print( "starting animal information extraction" )
 	for animal in mycursor:
 		animal_info.append( animal )
 	print( "done" )
@@ -45,7 +59,7 @@ def main():
 	while getting_info:
 	
 		animals_within_boundaries = []
-		print( "Enter longitude and latitude (comma separated) or \"exit\" to exit: " )
+		print( "Enter latitude and longitude (comma separated) or \"exit\" to exit: " )
 		response = input()
 		if response.lower() == "exit":
 			getting_info = False
@@ -61,34 +75,45 @@ def main():
 					#count += 1
 					#print( count, end= " " )
 					
-					if checkCoordinates_in_animalInfo( latitude, longitude, animal_boundaries[ index ], SEARCH_RADIUS ):
+					if checkCoordinates_in_animalInfo( longitude, latitude, animal_boundaries[ index ], SEARCH_RADIUS ):
 						animals_within_boundaries.append( animal_info[ index ] )
 								
 				if len( animals_within_boundaries ) == 0:
 					print( "There were no mammals in that area" )
 				else:
-					display_mammal_information( animals_within_boundaries, descriptors )
+					filename = write_mammal_info_to_csv( animals_within_boundaries, descriptors, latitude, longitude )
+					send_csv_to_drive( filename )
+					#display_mammal_information( animals_within_boundaries, descriptors )
 					print( "number of animals" )
 					print( len( animals_within_boundaries ) )
 			except ValueError: 
 				print( "Please input a valid latitude and longitude or \"Exit\" " )	
 	
 # end main 	
+
+# Check if an animal's habitat area is within the area we're searching 
 def checkCoordinates_in_animalInfo( latitude, longitude, animal_boundary, search_radius ):
 	
 	# create a polygon object originating from the latitude and longitude
 	origin_point = Point( latitude, longitude )
 	
+	# Create a circle that will be where we search for animal habitats
 	search_area = origin_point.buffer( search_radius )
 	
+	# Turn this circle into a polygon object that can be used to check if it
+	# intersects with an animal's polygon object that represents its habitat
 	search_polygon = Polygon( list( search_area.exterior.coords ) )
 
-	# return if it's within the animal polygon
+	# Check if the animal's boundary is unusable
 	if animal_boundary.is_valid == False:
+	
+		# If so, just return false 
 		return False
 	
+	# Otherwise, check if our search area intersects with the animal's habitat
 	return search_polygon.intersects( animal_boundary )
 
+# Print the animal's information to the commandline 
 def display_mammal_information( listOfMammals, descriptors ):
 
 	for x in listOfMammals: 
@@ -98,9 +123,51 @@ def display_mammal_information( listOfMammals, descriptors ):
 				print( x[ index ] )
 		
 		print( "\n====================\n" )
+
+# Write our found animals' information to a CSV file 
+def write_mammal_info_to_csv( listOfMammals, descriptors, latitude, longitude ):
+
+	# Variables
+	now = datetime.now() 
+	list_of_mammals_cleaned = []; 
+
+	# Create CSV file name 
+	# Add the date and time to ensure that the file names are unique
+	file_wo_extension = "mammal_info_" + now.strftime( "%d-%m-%Y %H-%M-%S" )
+	file_name = file_wo_extension + ".csv"
+	
+	# Create a CSV file to write to 
+	with open( file_name, mode='w' ) as csv_file: 
+		writer = csv.writer( csv_file )
 		
-# end display_mammal_information
+		# Write the latitude and longitude to the CSV
+		writer.writerow( [ latitude, longitude ] )
 		
+		# Delete the 17th column in the descriptors 
+		headers = list( descriptors )
+		headers.pop( 17 )	
+			
+		# Write the descriptors/headers to the file 
+		writer.writerow( headers )
+		
+		# Loop through each animal we found 
+		for animal in listOfMammals:
+			
+			# Create a list of the animal's info 
+			mammal_info = list( animal )
+			
+			# Delete the 17th index, the shape files 
+			mammal_info.pop( 17 )
+			
+			# Write the information to the row in the CSV
+			writer.writerow( mammal_info )	
+			
+	# Print to the user that the CSV file has been printed
+	print( "done writing to CSV file" )
+	
+	return file_wo_extension
+		
+# Creates a polygon object from our database's shape file parameters
 def create_polygon( currentShape ):
 
 	listOf_shapesPoints = []
@@ -154,6 +221,7 @@ def create_polygon( currentShape ):
 	except ValueError:
 		return Polygon( [(0,0), (0,0), (0,0 )] )
 		
+# Creates a multi-polygon object from our database's animal boundary information 
 def create_multi_polygon( currentShape ):
 	
 	# Get rid of the cruft from the database string 
@@ -167,20 +235,53 @@ def create_multi_polygon( currentShape ):
 	
 	return multi_polygon
 
+# Helper function that determines if a multi-polygon or polygon object needs to be created
 def create_shape( currentShape ):
+
+	# We only need the first index of our current shape
 	if currentShape[ 0 ] is not None: 
+	
+		# Check if the word, 'multipolygon' is in the boundary description
 		if "MULTIPOLYGON" in currentShape[ 0 ]:
-			#print( "	multi" )
-			#return( " " )
+		
+			# Create the multi-polygon and return it 
 			return create_multi_polygon( currentShape[ 0 ] )
 			
-			
+		# Check if the word, 'polygon' is in the boundary description 
 		elif "POLYGON" in currentShape[ 0 ]:
-			#print( " polygon" )
-			#return( " " )
+		
+			# Create the polygon and return it 
 			return create_polygon( currentShape[ 0 ] )
+			
+	# If there is an issue, return a polygon that is only a point 
 	return Polygon( [(0,0), (0,0), (0,0)] )
+
+def send_csv_to_drive( fileName ):
+
+	print( 'begin file upload' )
 	
+	# Create google account authentication objects
+	gauth = GoogleAuth()
+	
+	print( 'client secrets 0' )
+	gauth.LocalWebserverAuth()
+	
+	print( 'client secrets 1 ' )
+	
+	drive = GoogleDrive( gauth )
+	
+	print( 'client secrets 2 ' )
+	
+	upload_csv = drive.CreateFile({ fileName: fileName + '.csv' })
+	
+	print( 'client secrets 3' ) 
+	upload_csv.SetContentFile( fileName + '.csv' ) 
+	
+	print( 'client secrets 4' )
+	upload_csv.Upload() 
+	
+	print( 'file uploaded' )
+
 main()
 
 #
