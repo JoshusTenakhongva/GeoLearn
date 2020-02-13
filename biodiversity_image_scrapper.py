c58c2551b4feb185ead68a476fb7759765b16be8
JoshusTenakhongva <jtt75@nau.edu>
2020-02-12 21:54:16 -0700

    added google image search page functionality

--- biodiversity_image_scrapper.py
+++ biodiversity_image_scrapper.py
@@ -1,21 +1,39 @@
+# pip3 install selenium
+# sudo apt-get install firefox-geckodriver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-search_url = https://www.google.co.in/search?q="{search_query}"&source=lnms&tbm=isch 
+def main():
+    image_scraper( "fennec fox" )
 
-driver = webdriver.Firefox()
+def image_scraper( animal_search ):
 
-image_search = fennec fox
+    # Create our google image search url template 
+    search_url = "https://www.google.co.in/search?q={search_query}&source=lnms&tbm=isch"
 
-driver.get( search_url.format( search_query = image_serach ))
+    # Connect our python script to our firefox browser
+    driver = webdriver.Firefox()
 
-assert "Google" in driver.title
+    # Change any spaces in the search query into pluses
+    image_search = correct_for_query_spaces( animal_search )
 
-elem = driver.find_element_by_name( "q" )
-elem.clear()
-elem.send_keys( "pycon" )
-elem.send_keys( Keys.RETURN )
+    # Have our webdriver connect to our crafted url
+    # The url replaces the "search query" with our actual search query
+    driver.get( search_url.format( search_query = image_search ))
 
-assert "No results found." not in driver.page_source
+    # Check that the connection to the website was successful 
+    assert "Google" in driver.title
+    assert "No results found." not in driver.page_source
+
+    
 
 
+def correct_for_query_spaces( search_query ):
+    temp_query = search_query.split()
+    return '+'.join( temp_query )
+
+def retreive_image_urls( search_query ):
+    
+
+if __name__ == "__main__":
+    main()
